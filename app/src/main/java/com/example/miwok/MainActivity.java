f2ca23bf5776e1e694386de4b2ed1fb28308a646
Kritika6100 <jainkritika610@gmail.com>
2021-09-20 18:24:36 +0530

    https://github.com/Kritika6100/Miwok.git

--- app/src/main/java/com/example/miwok/MainActivity.java
+++ app/src/main/java/com/example/miwok/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.miwok;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import static com.example.miwok.R.id.numbers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,13 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView number=(TextView) findViewById(R.id.numbers);
+    number.setOnClickListener(new NumbersClickListener());
     }
 
 
-    public void OpenNumberslist(View view) {
-        Intent i = new Intent(this, NumbersActivity.class);
-        startActivity(i);
-    }
+
+
 
     public void OpenFamilylist(View view) {
         Intent i = new Intent(this, FamilyActivity.class);
